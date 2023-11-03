9cb6207965de4ad092b3484a095fd3bdcc4fd081
Naveen5676 <sample.first@yahoo.com>
2023-11-03 17:08:49 +0530

    Adding Event Listeners to the react

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -4,6 +4,13 @@ import "./ExpenseItems.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const clickHandler=()=>{
+    alert('clicked!!!!!!!!!!!')
+  }
+
+  const deleteHandler = () => {
+      alert('deleted')
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -16,6 +23,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <h2>{props.location}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteHandler}>Delete</button>
       </div>
     </Card>
   );
