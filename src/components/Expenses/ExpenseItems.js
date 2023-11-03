0f92a4f1c66871be353c8e3b8f1850c73e450eee
Naveen5676 <sample.first@yahoo.com>
2023-11-03 18:41:54 +0530

    use react hook i.e state (useState)

--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -2,15 +2,22 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetail from "./ExpenseDetail";
 import "./ExpenseItems.css";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const clickHandler=()=>{
-    alert('clicked!!!!!!!!!!!')
-  }
+  const [title, setTitle] = useState(props.title);
 
-  const deleteHandler = () => {
-      alert('deleted')
+  const [amount, setAmount] = useState(props.amount);
+
+  function clickHandler() {
+    setTitle("Update");
+    console.log(title);
+  }
+  
+  const amountHandler = () => {
+    setAmount("100");
   };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -20,14 +27,14 @@ const ExpenseItem = (props) => {
         amount={props.amount}
       />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h2>{props.location}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={amountHandler}>Change amount</button>
         <button onClick={clickHandler}>Change Title</button>
-        <button onClick={deleteHandler}>Delete</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
