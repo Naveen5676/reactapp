17aa396333acee9760917128cd48355cf1110a82
Naveen5676 <sample.first@yahoo.com>
2023-11-04 19:39:56 +0530

    Rendering Expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -2,47 +2,44 @@ import React, { useState } from "react";
 import Expense from "./components/Expenses/Expense";
 import NewExpenses from "./components/NewExpenses/NewExpenses";
 
+
+const DUMMY_EXPENSES = [{
+  id: "e1",
+  title: "Toilet Paper",
+  amount: 94.12,
+  date: new Date(2020, 7, 14),
+  location: "Dharwad",
+},
+{
+  id: "e2",
+  title: "New TV",
+  amount: 799.49,
+  date: new Date(2021, 2, 12),
+  location: "Hubli",
+},
+{
+  id: "e3",
+  title: "Car Insurance",
+  amount: 294.67,
+  date: new Date(2021, 2, 28),
+  location: "Gadag",
+},
+{
+  id: "e4",
+  title: "New Desk (Wooden)",
+  amount: 450,
+  date: new Date(2021, 5, 12),
+  location: "Udapi",
+},]
+
+
 const App = () => {
-  const [expenses, setExpenses] = useState([
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      location: "Dharwad",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      location: "Hubli",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      location: "Gadag",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      location: "Udapi",
-    },
-  ]);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expenseData) => {
-    // Create a new expense object with a unique ID and the provided data
-    const newExpense = {
-      id: Math.random().toString(), 
-      ...expenseData,
-    };
-
-    // Update the expenses state with the new expense
-    setExpenses((prevExpenses) => [newExpense, ...prevExpenses]);
+    setExpenses((prevExpenses)=>{
+      return[expenseData,...prevExpenses ];
+    });
   };
 
   return (
